8e0d317ad10cec42560004b98394d6e64f95ee59
mlag123 <mlag123@list.ru>
2026-05-24 19:53:50 +0700

    init

---
+++ kl5n-vendor.mk
@@ -0,0 +1,12 @@
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Auto-generated from proprietary-files.txt
+
+
+#позже разобратся что это вообще такое
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/tecno/kl5n/proprietary/vendor,$(TARGET_COPY_OUT_VENDOR))
