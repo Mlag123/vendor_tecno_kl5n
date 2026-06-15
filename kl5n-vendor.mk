166c8b00e2da7f4dd4db68211f0561ab7e90d2de
Mlag123 <disw123321@gmail.com>
2026-06-15 14:26:14 +0700

    refactor folders

--- kl5n-vendor.mk
+++ kl5n-vendor.mk
@@ -8,5 +8,14 @@
 
 #позже разобратся что это вообще такое
 
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/tecno/kl5n/proprietary/vendor,$(TARGET_COPY_OUT_VENDOR))
+# Copyright (C) 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Copy all proprietary vendor blobs
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/kl5n/proprietary/vendor,$(TARGET_COPY_OUT_VENDOR))
+
+# Copy system_ext blobs if exist
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/kl5n/proprietary/system_ext,$(TARGET_COPY_OUT_SYSTEM_EXT))
+
+# Copy product blobs if exist
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/kl5n/proprietary/product,$(TARGET_COPY_OUT_PRODUCT))
